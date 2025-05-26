a840710e2dd6de4072a3199682370ee891dcbe8a
Richard Huss <richard@huss.org.uk>
2025-05-26 15:16:27 +0100

    Initial commit

---
+++ constants.js
@@ -0,0 +1,55 @@
+module.exports = Object.freeze({
+
+    SIMPLE_SIGNALS: [
+        { id: 'captions:stream:start', label: 'Start streaming captions', buttonText: 'Start captions' },
+        { id: 'captions:stream:stop', label: 'Stop streaming captions', buttonText: 'Stop captions' },
+        { id: 'captions:stream:clear', label: 'Clear caption text', buttonText: 'Clear captions' },
+        { id: 'captions:pending:on', label: 'Turn on "Starting soon" message', buttonText: 'Starting soon' },
+        { id: 'captions:pending:off', label: 'Turn off "Starting soon" message', buttonText: 'Clear starting soon' },
+        { id: 'captions:visibility:hide', label: 'Temporarily hide captions', buttonText: 'Hide captions' },
+        { id: 'captions:visibility:show', label: 'Show captions', buttonText: 'Show captions' }
+    ],
+
+    // TODO: It would be lovely if this list could be obtained from CaptionKit rather than hard-coded in the module
+    LANGUAGES: [
+        { id: 'en', label: 'English' },
+        { id: 'en-US', label: 'English (US)' },
+        { id: 'en-AU', label: 'English (AU)' },
+        { id: 'en-GB', label: 'English (GB)' },
+        { id: 'en-NZ', label: 'English (NZ)' },
+        { id: 'bg', label: 'Bulgarian' },
+        { id: 'cs', label: 'Czech' },
+        { id: 'da', label: 'Danish' },
+        { id: 'nl', label: 'Dutch' },
+        { id: 'fr', label: 'French' },
+        { id: 'fr-CA', label: 'French (Canada)' },
+        { id: 'de', label: 'German' },
+        { id: 'de-CH', label: 'German (Switzerland)' },
+        { id: 'el', label: 'Greek' },
+        { id: 'hi', label: 'Hindi' },
+        { id: 'hu', label: 'Hungarian' },
+        { id: 'id', label: 'Indonesian' },
+        { id: 'it', label: 'Italian' },
+        { id: 'ko', label: 'Korean' },
+        { id: 'ja', label: 'Japanese' },
+        { id: 'lt', label: 'Lithuanian' },
+        { id: 'ms', label: 'Malay' },
+        { id: 'no', label: 'Norwegian' },
+        { id: 'pl', label: 'Polish' },
+        { id: 'pt', label: 'Portuguese' },
+        { id: 'pt-BR', label: 'Portuguese (Brazil)' },
+        { id: 'ro', label: 'Romanian' },
+        { id: 'ru', label: 'Russian' },
+        { id: 'es', label: 'Spanish' },
+        { id: 'es-419', label: 'Spanish (Latin America)' },
+        { id: 'sv', label: 'Swedish' },
+        { id: 'th', label: 'Thai' },
+        { id: 'tr', label: 'Turkish' },
+        { id: 'uk', label: 'Ukrainian' },
+        { id: 'vi', label: 'Vietnamese' },
+        { id: 'zh', label: 'Chinese (Mandarin, Simplified)' },
+        { id: 'zh-TW', label: 'Chinese (Mandarin, Traditional)' },
+        { id: 'zh-HK', label: 'Chinese (Cantonese, Traditional)' },
+        { id: 'multi', label: 'English & Spanish (Bilingual)' }
+    ]
+})
